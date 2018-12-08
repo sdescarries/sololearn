7a665885d177f62bf2f6d1d404be2d7156edb863
Simon Descarries <simon.descarries@vantrix.com>
2018-12-08 13:41:40 -0500

    Fixed scaling, make rover rotate instead of map, add rocks

--- javascript/rover/main.js
+++ javascript/rover/main.js
@@ -6,11 +6,12 @@ const work = {
   ms: 0.0,
   mz: 0.0,
   md: 0.0,
+  rocks: []
 };
 
 const commands = {
-  lf: () => { work.md = -1; },
-  rt: () => { work.md = +1; },
+  lf: () => { work.md = -5; },
+  rt: () => { work.md = +5; },
   fw: () => { work.ms = +1; },
   bk: () => { work.ms = -1; },
   up: () => { work.mz = +10; },
@@ -32,9 +33,23 @@ async function init() {
   }
 
   {
-    // A basic rover clipart
+    // The rover representation
     const image = work.rover = new Image();
-    image.src = 'https://image.flaticon.com/icons/png/512/124/124544.png';
+    image.src = 'http://icons.iconarchive.com/icons/graphicloads/polygon/256/upload-3-icon.png';
+  }
+
+  const rockImgs = [];
+
+  for (let src of [
+    'http://icons.iconarchive.com/icons/anton-gerasimenko/harry-potter/128/Philosophers-Stone-icon.png',
+    'http://icons.iconarchive.com/icons/raindropmemory/down-to-earth/512/G12-Rock-icon.png',
+    'http://icons.iconarchive.com/icons/archigraphs/eco-health/512/White-Stone-icon.png',
+    'http://icons.iconarchive.com/icons/archigraphs/eco-health/512/Eroded-Stone-icon.png',
+  ]) {
+    // Some rock
+    const image = new Image();
+    image.src = src;
+    rockImgs.push(image);
   }
 
   for (let ctl of ['lf', 'rt', 'fw', 'bk', 'up', 'dn']) {
@@ -48,6 +63,18 @@ async function init() {
       work.mz = 0.0;
       work.md = 0.0;
     };
+  }
+
+  for (let n = 0; n < 1000; n++) {
+    const rock = {
+      x: Math.random() * 2 - 1,
+      y: Math.random() * 2 - 1,
+      s: Math.random(),
+      a: Math.random() * 360,
+      img: rockImgs[Math.floor(Math.random() * rockImgs.length)],
+    };
+
+    work.rocks.push(rock);
   }
 
   doLayout();
@@ -96,10 +123,11 @@ function animate() {
     animId,
     marsbg,
     rover,
+    rocks,
     ctx,
     x, y, z, d,
     ms, mz, md,
-    time
+    time,
   } = work;
 
   if (animId) {
@@ -115,23 +143,39 @@ function animate() {
   }
   work.time = now;
 
+  let hz = z / 2;
+  let zx, zy;
+
+  // Map
+  zx = x * z - hz;
+  zy = y * z - hz;
+  ctx.drawImage(marsbg, zx, zy, z, z);
+
+  // Rocks
+  for (let rock of rocks) {
+
+    zx = (rock.x + x) * z - hz;
+    zy = (rock.y + y) * z - hz;
+    //    ctx.save();
+    //    ctx.rotate(rock.a*Math.PI/180);
+    ctx.drawImage(rock.img, zx, zy, z / 200, z / 200);
+    //    ctx.restore();
+  }
+
   ctx.save();
   ctx.rotate(d*Math.PI/180);
-
-  let hz = z / 2;
-  ctx.drawImage(marsbg, -hz + x, -hz + y, z, z);
-
-  ctx.restore();
 
   // Rover is 1/40 the scale of the map
   let rz = z / 40;
   hz = rz / 2;
   ctx.drawImage(rover, -hz, -hz, rz, rz);
 
+  ctx.restore();
+
   //console.log({ ms, mz, md });
 
-  work.x += ms * Math.cos((-d+90)*Math.PI/180);
-  work.y += ms * Math.sin((-d+90)*Math.PI/180);
+  work.x += ms * Math.cos((d+90)*Math.PI/180) / z;
+  work.y += ms * Math.sin((d+90)*Math.PI/180) / z;
   work.z += mz;
   work.d += md;
 }
