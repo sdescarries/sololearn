31fdf32e5cba3fa022790347dfd3b15341c11614
Simon Descarries <simon.descarries@vantrix.com>
2018-12-09 15:06:19 -0500

    Improve background, movement, rock placement, first draft proximity detection - faulty for now (WIP)

--- javascript/rover/main.js
+++ javascript/rover/main.js
@@ -1,21 +1,31 @@
 const work = {
+  // Rover position in % of whole map, 0:0 is origin
   x: 0.0,
   y: 0.0,
-  z: 2000,
+
+  // Viewing altitude or full map scale
+  z: 600,
+
+  // Rover direction in degree
   d: 0.0,
+
+  // Moving speeds
   ms: 0.0,
   mz: 0.0,
   md: 0.0,
+
+  // Database of rocks found on surface
   rocks: []
 };
 
+// Rover commands for buttons
 const commands = {
   lf: () => { work.md = -5; },
   rt: () => { work.md = +5; },
   fw: () => { work.ms = +1; },
   bk: () => { work.ms = -1; },
-  up: () => { work.mz = +10; },
-  dn: () => { work.mz = -10; },
+  up: () => { work.mz = 1.1; },
+  dn: () => { work.mz = 0.9; },
 };
 
 async function init() {
@@ -25,6 +35,12 @@ async function init() {
 
   // Adapt layout dynamically
   window.onresize = doLayout;
+
+  {
+    // Space background
+    const image = work.space = new Image();
+    image.src = 'https://ak4.picdn.net/shutterstock/videos/3014164/thumb/1.jpg';
+  }
 
   {
     // An actual satelite image of Mars
@@ -52,6 +68,27 @@ async function init() {
     rockImgs.push(image);
   }
 
+  for (let n = 0; n < 100; n++) {
+
+    let x = (Math.random() - 0.5);
+    let y = (Math.random() - 0.5);
+
+    let fx = 0.98;
+    let fy = Math.sqrt(0.25 - x*x) * 2 * fx;
+
+    x *= fx;
+    y *= fy;
+
+    const rock = {
+      x, y,
+      s: Math.random(),
+      a: Math.random() * 360,
+      img: rockImgs[Math.floor(Math.random() * rockImgs.length)],
+    };
+
+    work.rocks.push(rock);
+  }
+
   for (let ctl of ['lf', 'rt', 'fw', 'bk', 'up', 'dn']) {
     const el = document.querySelector(`#${ctl}`);
     el.onpointerdown = () => {
@@ -65,69 +102,42 @@ async function init() {
     };
   }
 
-  for (let n = 0; n < 1000; n++) {
-    const rock = {
-      x: Math.random() * 2 - 1,
-      y: Math.random() * 2 - 1,
-      s: Math.random(),
-      a: Math.random() * 360,
-      img: rockImgs[Math.floor(Math.random() * rockImgs.length)],
-    };
-
-    work.rocks.push(rock);
-  }
+  work.canvas = document.querySelector('canvas');
 
   doLayout();
   animate();
 }
 
-function rand(range = 1000) {
-  return Math.floor((Math.random() * range));
-}
-
 function doLayout() {
 
-  const ww = window.innerWidth;
-  const wh = window.innerHeight;
-  let dir, dim;
+  const {
+    innerWidth,
+    innerHeight,
+  } = window;
 
-  // Change orientation based on aspect ratio
-  if (ww > wh) {
-    dir = 'row';
-    dim = wh;
-  } else {
-    dir = 'column';
-    dim = ww;
-  }
+  const {
+    canvas,
+  } = work;
 
-  //console.log({ ww, wh, dir, dim });
-
-  const canvas = work.canvas = document.querySelector('canvas');
-  const body = document.querySelector('body');
-
-  body.style.flexDirection = dir;
-
-  canvas.width = dim;
-  canvas.height = dim;
-
-  work.dim = dim;
-  work.ctx = canvas.getContext('2d');
-
-  // move to the center of the canvas
-  work.ctx.translate(dim/2, dim/2);
+  canvas.width = innerWidth - 350;
+  canvas.height = innerHeight;
 }
 
 function animate() {
 
   const {
+    canvas,
     animId,
+    space,
     marsbg,
     rover,
     rocks,
-    ctx,
-    x, y, z, d,
-    ms, mz, md,
     time,
+    ms, mz, md,
+  } = work;
+
+  let {
+    x, y, z, d,
   } = work;
 
   if (animId) {
@@ -143,23 +153,90 @@ function animate() {
   }
   work.time = now;
 
+  const {
+    width,
+    height,
+  } = canvas;
+
+  // reset canvas for a fresh paint
+  canvas.width = width;
+  const ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#555';
+  ctx.fillRect(0, 0, width, height);
+
+  // Background
+  {
+    let iw = space.width;
+    let ih = space.height;
+    let ir = iw / ih;
+
+    if (ir > width / height) {
+      ih = height;
+      iw = height * ir;
+    } else {
+      iw = width;
+      ih = width * ir;
+    }
+    ctx.drawImage(space, 0, 0, iw, ih);
+  }
+
+
+  // move to the center of the canvas
+  ctx.translate(width/2, height/2);
+
   let hz = z / 2;
   let zx, zy;
 
   // Map
+
   zx = x * z - hz;
   zy = y * z - hz;
+
+  ctx.arc(zx + hz, zy + hz, hz * 0.97, 0, Math.PI * 2, true);
+  ctx.clip();
   ctx.drawImage(marsbg, zx, zy, z, z);
 
-  // Rocks
-  for (let rock of rocks) {
+  const proximity = {};
 
-    zx = (rock.x + x) * z - hz;
-    zy = (rock.y + y) * z - hz;
-    //    ctx.save();
-    //    ctx.rotate(rock.a*Math.PI/180);
-    ctx.drawImage(rock.img, zx, zy, z / 200, z / 200);
-    //    ctx.restore();
+  // Rocks
+  for (let idx = 0; idx < rocks.length; idx ++) {
+
+    const rock = rocks[idx];
+
+
+    hz = z / (rock.s * 100 + 150);
+    zx = (rock.x + x) * z - hz / 2;
+    zy = (rock.y + y) * z - hz / 2;
+    ctx.drawImage(rock.img, zx, zy, hz, hz);
+
+    const dx = rock.x - x;
+    const dy = rock.y - y;
+    const dist = Math.sqrt(dx*dx + dy*dy);
+
+    if (!proximity.dist || proximity.dist > dist) {
+      proximity.rock = rock;
+      proximity.dist = dist;
+    }
+  }
+
+  {
+
+    const { rock } = proximity;
+
+    if (work.closestRock !== rock) {
+      console.log(proximity);
+      work.closestRock = rock;
+    }
+
+    hz = z / (rock.s * 100 + 150);
+    zx = (rock.x + x) * z;
+    zy = (rock.y + y) * z;
+    ctx.beginPath();
+    ctx.arc(zx + hz, zy + hz, hz, 0, Math.PI * 2, true);
+    ctx.lineWidth=5;
+    ctx.strokeStyle = '#0F0';
+    ctx.stroke();
   }
 
   ctx.save();
@@ -172,10 +249,29 @@ function animate() {
 
   ctx.restore();
 
-  //console.log({ ms, mz, md });
+  let s = 0;
 
-  work.x += ms * Math.cos((d+90)*Math.PI/180) / z;
-  work.y += ms * Math.sin((d+90)*Math.PI/180) / z;
-  work.z += mz;
-  work.d += md;
+  if (ms < 0) {
+    s = Math.min(-0.01, ms / z);
+  } else if (ms > 0) {
+    s = Math.max(0.01, ms / z);
+  }
+
+  x += s * Math.cos((d+90)*Math.PI/180);
+  y += s * Math.sin((d+90)*Math.PI/180);
+  z *= mz;
+  d += md;
+
+  if (Math.sqrt(x*x + y*y) < 0.45) {
+    work.x = x;
+    work.y = y;
+  } else {
+    console.log('too close to edge of the world');
+  }
+
+  if (z > 100) {
+    work.z = z;
+  }
+
+  work.d = d;
 }
